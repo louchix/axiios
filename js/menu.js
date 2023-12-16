53b79417025ff8383cb32c0fb4d5157675bea867
LouchiX <alexcachon@free.fr>
2023-12-16 21:32:30 +0100

    Revert "rework taskbar and menu"
    This reverts commit 4a7c3c49cc7fce7150155dcfa0cda2e9dd57e086.

--- js/menu.js
+++ js/menu.js
@@ -1,41 +1,43 @@
-// Sélectionnez le bouton de la taskbar
-const startMenuButton = document.getElementById('start-menu-button');
-// Sélectionnez le menu
+// Récupérez l'élément du logo du menu
+const menuLogo = document.querySelector('.menu-logo');
+
+// Récupérez l'élément de l'heure
+const clockElement = document.getElementById('clock');
+
+// Récupérez l'élément de la date
+const dateElement = document.getElementById('date');
+
+// Récupérez l'élément du menu
 const menu = document.querySelector('.menu');
-// Sélectionnez le logo du menu
-const menuLogo = document.getElementById('menu-logo');
 
-// Vérifiez si les éléments sont correctement sélectionnés
-console.log('startMenuButton:', startMenuButton);
-console.log('menu:', menu);
-console.log('menuLogo:', menuLogo);
+// Écoutez le clic sur le logo pour ouvrir le menu
+menuLogo.addEventListener('click', () => {
+    // Ajoutez une classe pour afficher le menu (utilisez la classe 'menu-open' que vous avez définie précédemment)
+    menu.classList.add('menu-open');
+});
 
-// Fermez le menu par défaut en ajoutant la classe .menu-hidden
-menu.classList.add('menu-hidden');
-
-// Ajoutez un écouteur d'événements pour le clic sur le bouton de la taskbar
-startMenuButton.addEventListener('click', () => {
-    // Basculez la classe .menu-hidden pour afficher ou masquer le menu
-    menu.classList.toggle('menu-hidden');
-
-    // Affichez les propriétés CSS du menu en mode "open" dans la console
-    if (!menu.classList.contains('menu-hidden')) {
-        console.log('Menu Styles (Open):');
-        console.log('Width:', menu.clientWidth);
-        console.log('Height:', menu.clientHeight);
-        console.log('Opacity:', window.getComputedStyle(menu).getPropertyValue('opacity'));
-        console.log('Visibility:', window.getComputedStyle(menu).getPropertyValue('visibility'));
-        console.log('Background Color:', window.getComputedStyle(menu).getPropertyValue('background-color'));
-        console.log('Color:', window.getComputedStyle(menu).getPropertyValue('color'));
-        console.log('Z-Index:', window.getComputedStyle(menu).getPropertyValue('z-index'));
+// Écoutez le clic sur l'heure pour fermer le menu si ouvert
+clockElement.addEventListener('click', () => {
+    // Vérifiez si le menu est ouvert
+    if (menu.classList.contains('menu-open')) {
+        // Retirez la classe 'menu-open' pour fermer le menu
+        menu.classList.remove('menu-open');
     }
 });
 
-// Ajoutez un écouteur d'événements pour le clic en dehors du menu et du logo
-document.addEventListener('click', (e) => {
-    // Vérifiez si l'élément cliqué n'est pas le menu ni le logo
-    if (!menu.contains(e.target) && e.target !== menuLogo && e.target !== startMenuButton) {
-        // Si c'est le cas, masquez le menu en ajoutant la classe .menu-hidden
-        menu.classList.add('menu-hidden');
+// Écoutez le clic sur la date pour fermer le menu si ouvert
+dateElement.addEventListener('click', () => {
+    // Vérifiez si le menu est ouvert
+    if (menu.classList.contains('menu-open')) {
+        // Retirez la classe 'menu-open' pour fermer le menu
+        menu.classList.remove('menu-open');
     }
 });
+
+// Écoutez le clic en dehors du menu pour le fermer
+document.addEventListener('click', (event) => {
+    if (!menu.contains(event.target) && !menuLogo.contains(event.target) && !clockElement.contains(event.target) && !dateElement.contains(event.target)) {
+        // Retirez la classe 'menu-open' pour fermer le menu
+        menu.classList.remove('menu-open');
+    }
+}
